bb7db695a3a3f64b247d3823ad2244739552506b
xueshen.an <xueshen.an@goertek.com>
2023-10-08 13:15:10 +0800

    fixed copy file path error

--- build_qmake.sh
+++ build_qmake.sh
@@ -4,11 +4,13 @@
 
 rm -rf build
 mkdir build
-cd build
+pushd build
 
 #/usr/lib/aarch64-linux-gnu/qt5/bin/qmake ..
 /usr/local/Qt-5.15.10/bin/qmake ..
 
 make -j4
+
+popd
 
 cp lunch.sh build/release
